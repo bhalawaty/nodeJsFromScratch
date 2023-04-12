7dd593c1e961cda08098c12c868b7d4a630a9756
bhalawaty <bilal.halawaty@gmail.com>
2023-04-12 05:50:05 +0200

    add morgan dev middleware

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const morgan = require('morgan');
 const tourRouter = require(`${__dirname}/Routes/tourRoutes.js`);
-
 const app = express();
+
+// 1) MIDDLEWARES
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
 app.use(express.json());
 app.use(morgan('dev'));
-
 app.use('/v1/tours', tourRouter);
-
 
 module.exports = app;
