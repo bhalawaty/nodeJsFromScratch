21b8dc369f8deda7774259ba5cb25dda0fe05a29
bhalawaty <bilal.halawaty@gmail.com>
2023-04-17 06:23:22 +0200

    untracked fixed

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
 
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./Controllers/errorController');
 const tourRouter = require(`${__dirname}/Routes/tourRoutes.js`);
 const app = express();
 
@@ -10,5 +12,11 @@ if (process.env.NODE_ENV === 'development') {
 }
 app.use(express.json());
 app.use('/v1/tours', tourRouter);
+
+app.all('*', (req, res, next) => {
+    next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+app.use(globalErrorHandler);
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
+
+process.on('uncaughtException', err => {
+    console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+    console.log(err.name, err.message);
+    process.exit(1);
+});
+
+
 dotenv.config({path: './.env'});
 const app = require('./app');
 
@@ -15,9 +23,18 @@ mongoose
         useCreateIndex: true,
         useFindAndModify: false
     })
-    .then(() => console.log('DB connection successful!'));
+    .then(() => console.log('DB connection successful!')).catch(err => {
+    console.log("database connection error ");
+});
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
     console.log(`App running on port ${port}...`);
+});
+process.on('unhandledRejection', err => {
+    console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+    console.log(err.name, err.message);
+    server.close(() => {
+        process.exit(1);
+    });
 });
