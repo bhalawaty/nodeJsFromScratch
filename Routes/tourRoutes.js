a377cf714870674e07370a5159216860a8c351ed
bhalawaty <bilal.halawaty@gmail.com>
2023-04-17 04:00:12 +0200

    first create new database schema model for tour create crud opertaions for  tour and add some other apis for top tour and status too , create new import script to import tour data to mongno database

--- Routes/tourRoutes.js
+++ Routes/tourRoutes.js
@@ -1,17 +1,26 @@
 const express = require('express');
-const tourController = require(`${__dirname}/../Controllers/TourController.js`);
+const tourController = require('./../Controllers/TourController');
 
 const router = express.Router();
 
-router.param('id', tourController.checkID);
+// router.param('id', tourController.checkID);
 
 router
-  .route('/')
-  .get(tourController.getTours)
-  .post(tourController.checkBody, tourController.addTour);
+    .route('/top-5-cheap')
+    .get(tourController.aliasTopTours, tourController.getAllTours);
+
+router.route('/tour-stats').get(tourController.getTourStats);
+router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);
 
 router
-  .route('/:id')
-  .get(tourController.findTour);
+    .route('/')
+    .get(tourController.getAllTours)
+    .post(tourController.createTour);
+
+router
+    .route('/:id')
+    .get(tourController.getTour)
+    .patch(tourController.updateTour)
+    .delete(tourController.deleteTour);
 
 module.exports = router;
